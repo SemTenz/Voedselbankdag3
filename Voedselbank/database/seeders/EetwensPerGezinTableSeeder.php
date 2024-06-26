4a1127080356d6dc8bc058ace551b411d3c17e2d
Steff Brouwer <sbrouwer05@gmail.com>
2024-06-26 16:16:23 +0200

    eetwensenpergezin seeder aangemaakt

---
+++ Voedselbank/database/seeders/EetwensPerGezinTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EetwensPerGezinTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('eetwenspergezin')->insert([
+            [
+                'id' => 1, 
+                'gezinid' => 1,
+                'eetwensid' => 2,
+            ],
+            [
+                'id' => 2, 
+                'gezinid' => 2,
+                'eetwensid' => 4,
+            ],
+            [
+                'id' => 3, 
+                'gezinid' => 3,
+                'eetwensid' => 4,
+            ],
+            [
+                'id' => 4, 
+                'gezinid' => 4,
+                'eetwensid' => 3,
+            ],
+            [
+                'id' => 5, 
+                'gezinid' => 5,
+                'eetwensid' => 2,
+            ],
+        ]);
+    }
+}
