fe4f79ad6aca63352850009a08d20ba3272ef7d8
Bram <336796@student.mboutrecht.nl>
2024-06-26 17:06:52 +0200

    product model af

--- Voedselbank/app/Models/Product.php
+++ Voedselbank/app/Models/Product.php
@@ -6,12 +6,3 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = [
-        'naam', 'houdbaarheidsdatum', 'leverancier_id'
-    ];
-
-    public function leverancier()
-    {
-        return $this->belongsTo(Leverancier::class);
-    }
-}
